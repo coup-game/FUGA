07112f42c9cb42d23e0027eb41456085d0200903
nicecarrot <nicecarrot20@gmail.com>
2023-02-19 12:11:06 +0900

    submit의 axios 필요

--- frontend/src/Components/Reusable/Form/Signin.tsx
+++ frontend/src/Components/Reusable/Form/Signin.tsx
@@ -1,6 +1,17 @@
 import styles from '../../../Styles/Reusable/_signin.module.scss';
+import { useState ,FormEvent } from 'react';
 
 const Signin = () => {
+  const [login, setLogin] = useState({ id: "", pw: "" });
+
+  let {id, pw} = login;
+
+  // 로그인 버튼, Enter를 누를 시 시행
+  const onSubmit = async (e?: FormEvent<HTMLFormElement>) =>{
+    e?.preventDefault();
+    // API 폴더에 get_signin 작성
+    const res = await get_signin(login);
+  }
   return (
     <div className={styles.login}>
       <div>LOGIN</div>
